721f0423b0a723241fa626887da00f1fafd45337
Harthann <nca.eyraud@gmail.com>
2022-06-08 18:53:17 +0200

    mmodifying module for a simple hello world and adding rule

--- ex04/usb_mod.c
+++ ex04/usb_mod.c
@@ -4,49 +4,23 @@
 #include <linux/kernel.h>       /* Needed for  */
 #include <linux/init.h>         /* Needed for the macros */
 #include <linux/usb.h>
+#include <linux/usb/input.h>
+#include <linux/hid.h>
+
 
 MODULE_LICENSE("GPL");
 
 
-
-/* table of devices that work with this driver */
-static struct usb_device_id skel_table[] = {
-	{ .driver_info = 42},
-	{ }                      /* Terminating entry */
-};
-MODULE_DEVICE_TABLE(usb, skel_table);
-
-static int skel_probe(struct usb_interface *interface, const struct usb_device_id *id)
+static int __init hello_init(void)
 {
-	pr_info("Hello usb\n");
+	pr_info("Hello device\n");
 	return 0;
 }
 
-static struct usb_driver skel_driver = {
-	.name        = "usb_driver",
-	.probe       = skel_probe,
-	.id_table    = skel_table,
-};
-
-static int __init usb_skel_init(void)
+static void __exit hello_exit(void)
 {
-	int result;
-
-	/* register this driver with the USB subsystem */
-	result = usb_register(&skel_driver);
-	if (result < 0) {
-		pr_info("usb_register failed for the "__FILE__ "driver.Error number %d", result);
-		return -1;
-	}
-
-	return 0;
+	pr_info("Goodbye device\n");
 }
 
-static void __exit usb_skel_exit(void)
-{
-	/* deregister this driver with the USB subsystem */
-	usb_deregister(&skel_driver);
-}
-
-module_init(usb_skel_init);
-module_exit(usb_skel_exit);
+module_init(hello_init);
+module_exit(hello_exit);
